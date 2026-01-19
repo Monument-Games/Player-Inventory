8bb8407bf2905119a79d18b679118daff5a8f65d
Liveside <livesi5e@gmail.com>
2026-01-19 14:58:08 +0100

    Added config file

---
+++ Runtime/Config.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace MonumentGames.Config
+{
+    public virtual class Config : MonoBehaviour
+    {
+        public static Config cfg;
+
+        [Header("Controls")] 
+        public KeyCode pickUpKey = KeyCode.E;
+
+        [Header("Player Interaction")] 
+        public float interactionRange = 2f;
+
+        public void Awake()
+        {
+            if (Config.cfg != this && Config.cfg != null)
+                Destroy(this);
+            else
+                Config.cfg = this;
+        }
+    }
+} 
